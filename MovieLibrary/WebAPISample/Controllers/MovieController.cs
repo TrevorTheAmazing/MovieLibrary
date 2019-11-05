3c50ae4c4b432de3372bfd15757083908756840c
trevor clements <trevortheamazing@gmail.com>
2019-11-04 20:42:16 -0600

    finished http request and REST API user stories.  response collections included.  updated user stories.

--- MovieLibrary/WebAPISample/Controllers/MovieController.cs
+++ MovieLibrary/WebAPISample/Controllers/MovieController.cs
@@ -44,22 +44,95 @@ namespace WebAPISample.Controllers
             return Ok(movie);
         }
 
+        [HttpPost]
         // POST api/values
-        public void Post([FromBody]Movie value)
+        //public void Post(Movie movie)
+        public HttpResponseMessage Post([FromBody] Movie movie)
         {
             // Create movie in db logic
+
+            try
+            {
+                using (var tempDb = new ApplicationDbContext())
+                {
+                    //tempDb.Movies.Add(new Movie()
+                    //{
+                    //    MovieId = movie.MovieId,
+                    //    Title = movie.Title,
+                    //    Director = movie.Director,
+                    //    Genre = movie.Genre
+                    //});
+
+                    db.Movies.Add(movie);
+
+                    db.SaveChanges();
+
+                    var message = Request.CreateResponse(HttpStatusCode.Created, movie);
+                    message.Headers.Location = new Uri(Request.RequestUri + movie.MovieId.ToString());
+                    //return Ok(movie.MovieId);
+                    return message;
+                }
+            }
+            catch(Exception e)
+            {
+                //Console.WriteLine(e.Message);
+                //return BadRequest();
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, e);
+            }
+            
+            
         }
 
         // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
+        public IHttpActionResult Put(Movie updatedMovieInformation)
         {
             // Update movie in db logic
+            Movie movieFromDatabase = null;
+
+            try
+            {
+                using (var tempDb = new ApplicationDbContext())
+                {
+
+
+                    movieFromDatabase = db.Movies.Where(c => c.MovieId == updatedMovieInformation.MovieId).SingleOrDefault();
+
+                    movieFromDatabase.Title = updatedMovieInformation.Title;
+                    movieFromDatabase.Director = updatedMovieInformation.Director;
+                    movieFromDatabase.Genre = updatedMovieInformation.Genre;
+
+                    db.SaveChanges();
+                }
+                return Ok();
+            }
+            catch(Exception e)
+            {
+                return NotFound();
+            }
         }
 
         // DELETE api/values/5
         public void Delete(int id)
         {
             // Delete movie from db logic
+            try
+            {
+                var tempMovie = db.Movies.Find(id);
+                
+                if (tempMovie != null)
+                {
+                    db.Movies.Remove(tempMovie);
+                    db.SaveChanges();
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+            
+
+            
         }
     }
 
